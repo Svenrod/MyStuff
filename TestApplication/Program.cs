eab433df6cb77bf56a7759d94e8d0d9929a8ac94
Svenrod <sven.rodriguez@hotmail.com>
2016-03-16 11:26:30 +0100

    Added 2nd hello world

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World Again!");
         }
     }
 }
